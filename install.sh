0ccd065e0613aa771841dcdd4dddcc3ae1fab726
xavatar94 <xavatar@wanadoo.fr>
2017-12-30 19:02:14 +0100

    fix phpmyadmin

--- install.sh
+++ install.sh
@@ -188,18 +188,19 @@ echo "Installation phpMyAdmin :"
 echo ""
 sleep 3
 
-if [[ ("$PHP7" == "y" || "$PHP7" == "Y") ]]; then
-	sudo mkdir /var/www/
-	sudo mkdir /var/www/html
-	sudo wget https://files.phpmyadmin.net/phpMyAdmin/4.7.7/phpMyAdmin-4.7.7-all-languages.tar.gz
-	sudo tar xvfz phpMyAdmin-4.7.7-all-languages.tar.gz
-	sudo mv phpMyAdmin-4.7.7-all-languages phpmyadmin
-	sudo mv phpmyadmin/ /var/www/html/
-	chown -R nobody.nobody /var/www/html/phpmyadmin
-	sudo cp /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
-else
-	sudo yum -y install phpMyAdmin
+if [[ ("$WEBSERV" == "y" || "$WEBSERV" == "Y") ]]; then
+		sudo yum -y install phpMyAdmin
+	else
+		sudo mkdir /var/www/
+		sudo mkdir /var/www/html
+		sudo wget https://files.phpmyadmin.net/phpMyAdmin/4.7.7/phpMyAdmin-4.7.7-all-languages.tar.gz
+		sudo tar xvfz phpMyAdmin-4.7.7-all-languages.tar.gz
+		sudo mv phpMyAdmin-4.7.7-all-languages phpmyadmin
+		sudo mv phpmyadmin/ /var/www/html/
+		chown -R nobody.nobody /var/www/html/phpmyadmin
+		sudo cp /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
 fi
+
 
 sudo touch /var/www/html/info.php
 sudo echo "<?php phpinfo(); ?>" |sudo tee  /var/www/html/info.php
