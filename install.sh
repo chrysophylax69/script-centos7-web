8797b6374896ad38d050c9b448d40ca02bed8091
xavatar94 <xavatar@wanadoo.fr>
2017-12-31 00:44:01 +0100

    fix typo

--- install.sh
+++ install.sh
@@ -142,6 +142,7 @@ sudo git clone https://github.com/websupport-sk/pecl-memcache.git
 sudo cd pecl-memcache && phpize && ./configure --disable-memcache-sasl && sudo make && sudo make install
 #sudo echo 'extension=memcached.so' >> /etc/php.ini
 sudo echo 'extension=memcache.so' >> /etc/php.ini
+cd ..
 echo ""
 echo "Vérification de Memcached :"
 sudo php -m | grep memcache
